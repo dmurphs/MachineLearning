c2fd374eb038eeaf19340891ecfbf2b3d8673317
unknown <d.p.murph19@gmail.com>
2016-03-29 18:51:34 -0600

    finished g function for BayesNet project

--- BayesNet/PythonScripts/helpers.py
+++ BayesNet/PythonScripts/helpers.py
@@ -1,35 +1,40 @@
 from math import factorial as fac
 
-num_dim_values = 2
-positive = 1
-negative = 0
-
 def get_dim_measurements(records,dimension):
-    num_child_pos = len([rec for rec in records if rec[dimension] == positive])
-    num_child_neg = len([rec for rec in records if rec[dimension] == negative])
+    num_child_pos = len([rec for rec in records if rec[dimension] == 1])
+    num_child_neg = len([rec for rec in records if rec[dimension] == 0])
     return {'positive': num_child_pos, 'negative': num_child_neg}
+
+def random_match_prob(N,r):
+    return fac(r-1)/float(fac(N+r-1))
+
+def upward_adjustment(num_pos,num_neg):
+    return fac(num_pos)*fac(num_neg)
 
 def g(data,dimension,parent_dimensions=[]):
     '''Calculates the parent probability'''
-
-    random_match_prob = lambda x: fac(num_dim_values - 1)/float(fac(x + num_dim_values - 1))
-    upward_adjustment = lambda x: fac(x['positive'])*fac(x['negative'])
-
+    num_unique_instatiations = 2
+    r = num_unique_instatiations
     if len(parent_dimensions) == 0:
-        measurements = get_dim_measurements(data,dimension)
-        return random_match_prob(len(data))*upward_adjustment(measurements)
-
+        dim_measurments = get_dim_measurements(data,dimension)
+        rand_match_prob = random_match_prob(len(data),r)
+        upward_adj = upward_adjustment(dim_measurments['positive'],dim_measurments['negative'])
+        return rand_match_prob*upward_adj
     else:
-        parent_child_match = lambda record,desired_val: record[dimension] == desired_val and record[parent_dimension] == desired_val
+        all_products = [];
+        print parent_dimensions
+        for p_dim in parent_dimensions:
+            positive_data = [rec for rec in data if rec[p_dim] == 1]
+            positive_children_measurments = get_dim_measurements(positive_data,dimension)
+            pos_rand_match_prob = random_match_prob(len(positive_data),r)
+            pos_upward_adj = upward_adjustment(positive_children_measurments['positive'],positive_children_measurments['negative'])
+            pos_prob = pos_rand_match_prob*pos_upward_adj
 
-        positive_parents = [rec for rec in data if rec[parent_dimension] == positive]
-        parent_positive = get_dim_measurements(positive_parents,dimension)
+            negative_data = [rec for rec in data if rec[p_dim] == 0]
+            negative_children_measurments = get_dim_measurements(negative_data,dimension)
+            neg_rand_match_prob = random_match_prob(len(negative_data),r)
+            neg_upward_adj = upward_adjustment(negative_children_measurments['positive'],negative_children_measurments['negative'])
+            neg_prob = neg_rand_match_prob*neg_upward_adj
 
-        negative_parents = [rec for rec in data if rec[parent_dimension] == negative]
-        parent_negative = get_dim_measurements(negative_parents,dimension)
-
-
-        p1 = random_match_prob(len(positive_parents))*upward_adjustment(parent_positive)
-        p2 = random_match_prob(len(negative_parents))*upward_adjustment(parent_negative)
-
-        return p1*p2
+            all_products.append(pos_prob*neg_prob)
+        return reduce(lambda x,y: x*y,all_products)
