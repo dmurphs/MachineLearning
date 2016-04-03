6300a3ff672ebf8155d69a3f137417daa4c0841e
Dan Murphy <d.p.murph19@gmail.com>
2016-04-03 13:36:06 -0600

    began working on classification component

--- BayesNet/PythonScripts/helpers.py
+++ BayesNet/PythonScripts/helpers.py
@@ -1,6 +1,7 @@
 from math import factorial as fac
 from operator import itemgetter
 from collections import Counter
+from itertools import permutations
 
 def get_dim_measurements(data,dimension):
     '''Get number of positive and negative measurements for dimension'''
@@ -36,8 +37,8 @@ def g(data,dimension,parent_dimensions=[]):
         all_products = [p_dim_prob_calc(p_dim,1)*p_dim_prob_calc(p_dim,0) for p_dim in parent_dimensions];
         return reduce(lambda x,y: x*y,all_products)
 
-def k2(data,u,column_ordering):
-    '''Find the most probable parents for a given dimension'''
+def k2(data,u,dim_ordering):
+    '''Find belief network structure for given ordering'''
     dimensions = data.columns
     dim_parents = {}
     for dim in dimensions:
@@ -45,16 +46,54 @@ def k2(data,u,column_ordering):
         p_old = g(data,dim,pi_i)
         ok = True
         while ok and len(pi_i) < u:
-            potential_parents = [p_dim for p_dim in dimensions if p_dim != dim and p_dim not in pi_i]
-            new_p_vals = {p_dim: g(data,dim,pi_i + [p_dim]) for p_dim in potential_parents}
-            print dim
-            print p_old
-            print new_p_vals
-            p_new,p_new_val = max(new_p_vals.iteritems(),key=itemgetter(1))
-            if p_new_val > p_old:
-                p_old = p_new_val
-                pi_i += [p_new]
+            is_pred = lambda p_dim: dim_ordering.index(p_dim) < dim_ordering.index(dim) and p_dim not in pi_i
+            preds = [p_dim for p_dim in dim_ordering if is_pred(p_dim)]
+            new_p_vals = {p_dim: g(data,dim,pi_i + [p_dim]) for p_dim in preds}
+            p_new_dim,p_new = max(new_p_vals.iteritems(),key=itemgetter(1)) if len(new_p_vals) > 0 else (None,0)
+            if p_new > p_old:
+                p_old = p_new
+                pi_i += [p_new_dim]
             else:
                 ok = False
         dim_parents[dim] = pi_i
     return dim_parents
+
+def k2_all_orderings(data,dimensions):
+    perms = permutations(dimensions)
+    structures = [k2(data,3,perm) for perm in perms]
+    return structures
+
+def find_all_parent_val_combinations(num_dims,possible_values,all_combs=[]):
+    if len(all_combs) == 2**num_dims or num_dims == 0:
+        return all_combs
+
+    new_combs_with_val = lambda val: [comb + [val] for comb in all_combs]
+    new_combs = [[val] for val in possible_values] if len(all_combs) == 0 else reduce(lambda x,y: x + y,[new_combs_with_val(val) for val in possible_values])
+    return find_all_parent_val_combinations(num_dims,possible_values,new_combs)
+
+def find_matching_data(data,dims,value_combination):
+    '''Get data from data that matches the combination of values from the given dimensions'''
+    matching_data = data
+    for dim in dims:
+        matching_data = matching_data[matching_data[dim] == value_combination[dim]]
+    return matching_data
+
+def get_prob_table(data,dim,p_dims):
+    '''Construct dictionary to represent table of conditional probability of dimension values given parent values'''
+    dim_counts = Counter(data[dim])
+    possible_values = dim_counts.keys()
+    get_prob = lambda data,dim_val: len(data[data[dim] == dim_val])/float(len(data))
+    if len(p_dims) == 0:
+        return {dim_val: [(get_prob(data,dim_val),{})] for dim_val in possible_values}
+    parent_val_combs = find_all_parent_val_combinations(len(p_dims),possible_values)
+    get_mapped_comb = lambda comb: {p_dims[i]: comb[i] for i in range(len(p_dims))}
+    mapped_combs = [get_mapped_comb(comb) for comb in parent_val_combs]
+    get_all_probs = lambda dim_val: [(get_prob(find_matching_data(data,p_dims,parent_comb),dim_val),parent_comb) for parent_comb in mapped_combs]
+    return {dim_val: get_all_probs(dim_val) for dim_val in possible_values}
+
+def classify_record(data,test_record,network,target_class,possible_values):
+    '''Classify a record'''
+    for dim in network:
+        p_dims = network[dim]
+        p_dim_comb = {p_dim: test_record[p_dim] for p_dim in p_dims}
+        table = get_prob_table(data,dim,p_dims)
--- BayesNet/PythonScripts/test.py
+++ BayesNet/PythonScripts/test.py
@@ -1,4 +1,4 @@
-from helpers import g,k2
+from helpers import g,k2,find_all_parent_val_combinations,get_prob_table,classify_record
 import pandas as pd
 
 test_records = [
@@ -28,4 +28,17 @@ print 'Expect about .0003607'
 print g(test_df,'x1')
 print '\n'
 
-print k2(test_df,3)
+print 'Checking k2 with order x1,x2,x3'
+print k2(test_df,3,list(test_df.columns))
+print '\n'
+
+print 'Testing find all combinations'
+print find_all_parent_val_combinations(len(test_df.columns),[0,1])
+print '\n'
+
+print 'Testing probability table construction'
+print get_prob_table(test_df,'x3',['x2'])
+print '\n'
+
+print 'Testing classification of test record'
+print classify_record(test_df,{'x1': 0, 'x2': 0, 'x3': 1},{'x2': ['x1'], 'x3': ['x2'], 'x1': []},'x1',[0,1])
