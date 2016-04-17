f9133eb43b473fb698d3078a4949931802faa0ab
Dan Murphy <d.p.murph19@gmail.com>
2016-04-17 11:21:05 -0600

    added functionality to be able to set random orderings for networks and pick best scoring network

--- BayesNet/PythonScripts/bayesnet.py
+++ BayesNet/PythonScripts/bayesnet.py
@@ -1,14 +1,15 @@
-from helpers import k2,get_matching_records,find_matching_data
+from helpers import k2,get_matching_records,find_matching_data,find_class_prob,get_best_network
 from random import randint,shuffle
 from csv import DictReader
 from operator import itemgetter
+from itertools import permutations
 
 with open('../Data/forestFireData.csv','r') as f:
     reader = DictReader(f)
     convert_to_ints = lambda rec: {dim: int(rec[dim]) for dim in rec}
     data = [convert_to_ints(rec) for rec in list(reader)]
 
-dimensions = ['Storms','BusTourGroup','Lightning','Campfire','Thunder']
+classification_dims = ['Storms','BusTourGroup','Lightning','Campfire','Thunder']
 target_class_values = [0,1]
 
 positive_records = get_matching_records(data,'Class',1)
@@ -26,19 +27,9 @@ shuffle(bootstrapped_data)
 k = 10
 
 subset_size = len(bootstrapped_data)/k
-classification_dims = [dim for dim in dimensions]
 
-def find_class_prob(record,train_set,network,target_dim,class_val,dims):
-    probs = []
-    for dim in dims:
-        #find probability dim value indicates class_val given its parents
-        p_dims = network[dim]
-        p_vals = {d: record[d] for d in p_dims}
-        matching_parents = find_matching_data(train_set,p_dims,p_vals)
-        matching_class_and_parents = [rec for rec in matching_parents if rec[target_dim] == class_val and rec[dim] == record[dim]]
-        prob = (1 + len(matching_class_and_parents))/float(1000 + len(matching_parents))
-        probs.append(prob)
-    return reduce(lambda x,y: x*y,probs)
+try_random_orderings = False
+num_k2_random_orderings = 90
 
 fold_accuracies = {}
 for i in range(k):
@@ -50,7 +41,14 @@ for i in range(k):
     head_indices = [j for j in range(0,test_start_index)]
     tail_indices = [j for j in range(test_end_index,len(bootstrapped_data)-1)]
     train_set = [bootstrapped_data[j] for j in head_indices + tail_indices]
-    network = k2(train_set,3,classification_dims)
+
+    if try_random_orderings:
+        all_perms = list(permutations(classification_dims))
+        shuffle(all_perms)
+        network = get_best_network(train_set,3,all_perms[:num_k2_random_orderings])
+    else:
+        network = k2(train_set,5,classification_dims)[0]
+
     for node in network:
         print 'Node %s has parents %s' %(node,str(network[node]))
     correct = 0
@@ -58,7 +56,7 @@ for i in range(k):
     for record in test_set:
         class_probs = {}
         for class_value in target_class_values:
-            class_prob = find_class_prob(record,train_set,network,'Class',class_value,dimensions)
+            class_prob = find_class_prob(record,train_set,network,'Class',class_value,classification_dims)
             class_probs[class_value] = class_prob
         guess = max(class_probs.iteritems(),key=itemgetter(1))[0]
         total += 1
--- BayesNet/PythonScripts/helpers.py
+++ BayesNet/PythonScripts/helpers.py
@@ -47,8 +47,9 @@ def g(data,dimension,parent_dimensions=[]):
         return reduce(lambda x,y: x+y,all_products)
 
 def k2(data,u,dim_ordering):
-    '''Find belief network structure for given ordering'''
+    '''Find belief network structure for given ordering along with network score'''
     dim_parents = {}
+    all_probs = []
     for dim in dim_ordering:
         pi_i = []
         p_old = g(data,dim,pi_i)
@@ -63,10 +64,12 @@ def k2(data,u,dim_ordering):
                 pi_i += [p_new_dim]
             else:
                 ok = False
+        all_probs.append(p_old)
         dim_parents[dim] = pi_i
-    return dim_parents
+    return dim_parents,sum(all_probs)
 
 def find_all_parent_val_combinations(num_dims,possible_values,all_combs=[]):
+    '''Recursively get all possible combinations for parents'''
     if len(all_combs) == 2**num_dims or num_dims == 0:
         return all_combs
 
@@ -80,3 +83,21 @@ def find_matching_data(data,dims,value_combination):
     for dim in dims:
         matching_data = get_matching_records(data,dim,value_combination[dim])
     return matching_data
+
+def find_class_prob(record,train_set,network,target_dim,class_val,dims):
+    '''Find the probability that a record belongs to a class'''
+    probs = []
+    for dim in dims:
+        #find probability dim value indicates class_val given its parents
+        p_dims = network[dim]
+        p_vals = {d: record[d] for d in p_dims}
+        matching_parents = find_matching_data(train_set,p_dims,p_vals)
+        matching_class_and_parents = [rec for rec in matching_parents if rec[target_dim] == class_val and rec[dim] == record[dim]]
+        prob = (1 + len(matching_class_and_parents))/float(1000 + len(matching_parents))
+        probs.append(prob)
+    return reduce(lambda x,y: x*y,probs)
+
+def get_best_network(data,u,orderings):
+    '''Get the best network from several different orderings'''
+    networks = [k2(data,u,ordering) for ordering in orderings]
+    return max(networks,key=itemgetter(1))[0]
